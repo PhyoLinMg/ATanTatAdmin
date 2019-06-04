280b512cbef940ebf44180aa162c3eb36aa0ad2e
PhyoLinMg <gangsterplm@gmail.com>
2019-06-04 19:52:02 +0630

    changed

--- config/database.php
+++ config/database.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -70,7 +70,7 @@ return [
             'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'password' => $DATABASE_URL["pass"], 
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
