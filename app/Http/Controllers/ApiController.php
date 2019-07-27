311cc12af6bad913332e1e37b7e12bc5f13a3b4d
PhyoLinMg <gangsterplm@gmail.com>
2019-07-27 20:08:27 +0630

    gg

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -64,6 +64,6 @@ class ApiController extends Controller
     }
     public function updateRollCall(Request $request){
         $gg=$request;
-        return response()->json(["message"=>(string)$gg]);
+        return response()->json(["message"=>(string)$request]);
     }
 }
